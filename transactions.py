0d2e576ccb54fab5c1d8f64d749c0cf771e68f33
U-K-06 <utkarsh7khajuria8@gmail.com>
2024-11-12 19:31:39 +0530

    divided the class into two sub classes

--- transactions.py
+++ transactions.py
@@ -11,7 +11,7 @@ class UserAccount:
         self.user_name:str = user_name
         self.total_money:int|None = 0
         self.reason:str|None = None
-        self.transactions:dict[str:list[int],str:list[str]]|None  = {'transactions':[],'reason':[],'Date':[]}
+        self.transactions:dict[str:list[int],str:list[str],str:list[date],str:list[str]] = {'Transactions':[],'Reason':[],'Date':[],'Type':[]}
         self.stats = {}
 
     def __str__(self)->str:
@@ -23,13 +23,19 @@ class UserAccount:
         else:
             pd.DataFrame(self.transactions).to_csv(path.joinpath(path,file_name),mode='a')
 
-    def add_transaction(self,transaction,reason)->None:
-        self.transactions['transactions'].append(transaction)
-        self.transactions['reason'].append(reason)
+    def add_transaction(self,transaction:str,reason:str,transaction_type:str)->None:
+        self.transactions['Transactions'].append(transaction)
+        self.transactions['Reason'].append(reason)
         self.transactions['Date'].append(date.today())
-        
+        self.transactions['Type'].append(transaction_type.capitalize())
 
-        
+class TransactionMangament:
+    def __init__(self):
+        self.accounts:dict[str:str] = {}
+    def add_account(self,user_name):
+        self.accounts[user_name] = UserAccount(user_name)
+    
 
 
-
+if __name__ == '__main__':
+    
