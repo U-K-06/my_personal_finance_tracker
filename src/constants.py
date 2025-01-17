96b2758fb778bbffe881a977323cc5b0900e110c
U-K-06 <utkarsh7khajuria8@gmail.com>
2025-01-18 02:48:01 +0530

    maybe progress?

--- src/constants.py
+++ src/constants.py
@@ -4,6 +4,8 @@ class Constants:
     PATH_CURRENT:path = path.cwd()
     DUMMY_CSV_PATH:path = './external_data/data.csv'
     JSON_IDS_PATH:path = './external_data/ids.json'
+    USER_ACCOUNT_PATH:path = './external_data/user_account.json'
     START_RANDOM_IDS:int = 10000
     END_RANDOM_IDS:int = 100000
+    DATA_BODY:{str,list}= {'Amount':[],'Reason':[],'Date':[],'Type':[]}
 
--- src/transactions.py
+++ src/transactions.py
@@ -4,41 +4,45 @@ from pathlib import Path as path
 from datetime import date
 from constants import Constants
 import Generate_id
+import json
+
 class UserAccount:
     def __init__(self,user_name:str)->None:
         self.user_name:str = user_name
-        self.transactions:pd.DataFrame= pd.DataFrame({'Amount':[],'Reason':[],'Date':[],'Type':[]})
+        self.transactions:pd.DataFrame= pd.DataFrame(Constants.DATA_BODY)
         self.id = Generate_id.generate_random_id()
         
-
 
     def __str__(self)->str:
         return self.transactions.to_string() if not self.transactions.empty  else "Account Data is Empty!"
     
     
-    def save_data(self,file_name=None,path=Constants.PATH_CURRENT)->None:
-        if path == Constants.PATH_CURRENT: #check weather the file in the cwd or if the path needs to made dynamically
-             self.add_transaction.to_csv(file_name,mode='a')
-        else:
-            self.transactions.to_csv(path.joinpath(path,(file_name or self.user_name+'_user_account_data'),mode='a'))
-
+    def save_data(self)->None:
+        data = {"ID":"",
+                "Name":"",
+                } | self.transactions.to_dict(orient="list")
+        print(data)
+        # with open(Constants.USER_ACCOUNT_PATH,'r') as user_account:
+        #     json_data = json.load(user_account)
+        # print(json_data)
+        
     def add_transaction(self,transaction:str,reason:str,transaction_type:str)->None:
-        self.transactions['Amount']
-        self.transactions['Reason'].combine(reason)
-        self.transactions['Date'].combine(date.today())
-        self.transactions['Type'].combine(transaction_type.capitalize())
+        data = Constants.DATA_BODY
+        data['Amount'].append(transaction)
+        data['Reason'].append(reason)
+        data['Date'].append(date.today())
+        data['Type'].append(transaction_type)
+        self.transactions = pd.concat([self.transactions,pd.DataFrame(data)],ignore_index=True)
     
     def get_stats(self)->None:
         return f'''Total Spent: {self.transactions[self.transactions['Type'] == "Given"]}'''
 
-# class TransactionMangament:
-#     def __init__(self):
-#         self.accounts:dict[str:str] = {}
-#     def add_account(self,user_name:UserAccount):
-#         self.accounts[user_name.user_name] = UserAccount(user_name)
+
 
 if __name__ == '__main__':
     a = UserAccount("JOHN DOE")
-    a.add_transaction(50,"Pens","Given")
-    a.add_transaction(60,"Food","Given")
-    print(a)
+    # a.add_transaction(50,"Pens","Given")
+    # a.add_transaction(60,"Food","Given")
+    a.add_transaction(400,"food","Given")
+    a.add_transaction(200,"pens","spent")
+    a.save_data()
